853b47cdd5c0a6985fda2012760cf161f8d27fdc
ccolin84 <ccolin84@gmail.com>
2017-09-27 16:46:41 -0400

    add small update to styling

--- client/src/components/QuizContainer.js
+++ client/src/components/QuizContainer.js
@@ -15,7 +15,8 @@ class QuizContainer extends Component {
   handleNavButtonClick(incriment) {
     this.setState({ 
       currentQuestionIndex: this.state.currentQuestionIndex + incriment,
-      userHasGuessed: false 
+      userHasGuessed: false,
+      realHeadlineInx: Math.floor(Math.random() * 3)
     })
   }
 
@@ -23,33 +24,40 @@ class QuizContainer extends Component {
     if (!this.state.userHasGuessed) this.setState({ userHasGuessed: true })
   }
 
+  componentWillReceiveProps() {
+    this.setState({ userHasGuessed: false });
+  }
+
+  componentDidMount() {
+    this.setState({ realHeadlineInx: Math.floor(Math.random() * 3)});
+  }
+
   render() {
     if (this.props.questionQueue.length) {
       let fakeHeadlines = this.props.questionQueue[this.state.currentQuestionIndex].fakeHeadlines;
       let realHeadline = this.props.questionQueue[this.state.currentQuestionIndex].realHeadline;
-      let randomRealHeadlineInx = Math.floor(Math.random() * 3);
       let mixedHeadlines = fakeHeadlines.slice();
-      mixedHeadlines.splice(randomRealHeadlineInx, 0, realHeadline.title);
+      mixedHeadlines.splice(this.state.realHeadlineInx, 0, realHeadline.title);
       return (
         <div id="quizContainer">
           <div id='questionsContainer'>
             <h3>One of These Headlines is Legit</h3>
             <ListGroup>
               { mixedHeadlines.map((headline, inx) => (
-                <ListGroupItem 
+                <ListGroupItem className='possibleHeadline'
                   onClick={ this.handleUserChoice }
-                  bsStyle={ this.state.userHasGuessed ? (inx === randomRealHeadlineInx ? 'success' : 'danger') : ''}
+                  bsStyle={ this.state.userHasGuessed ? (inx === this.state.realHeadlineInx ? 'success' : 'danger') : ''}
                   key={ headline + inx }>
                   { headline }
                 </ListGroupItem>
               ))}
             </ListGroup>
             <div id="gameButtons">
-              <Button onClick={ () => this.handleNavButtonClick(-1) } 
+              <Button bsSize='large' onClick={ () => this.handleNavButtonClick(-1) } 
                 disabled={ this.state.currentQuestionIndex === 0 ? true : false }>
                 Back
               </Button>
-              <Button onClick={ () => this.handleNavButtonClick(1) }
+              <Button bsSize='large' onClick={ () => this.handleNavButtonClick(1) }
                 disabled={ this.state.currentQuestionIndex === (this.props.questionQueue.length - 1) ? true : false }>
                 Next
               </Button>
@@ -58,12 +66,12 @@ class QuizContainer extends Component {
           { 
             !this.state.userHasGuessed ? '' : 
             <div id='articleInfo'>
-              <img src={ realHeadline.urlToImage || 'pexels-photo-242492.jpeg' } style={{ height: '35%', width: '35%'}}/>
+              <img src={ realHeadline.urlToImage } style={{ height: '35%', width: '35%'}}/>
               <h4>{ realHeadline.author }</h4>
               <h3>{ realHeadline.title }</h3>
               <div>{ realHeadline.publishedAt }</div>
               <div>{ realHeadline.description }</div>
-              <div><a href={ realHeadline.url }>{ realHeadline.url }</a></div>
+              <div><a href={ realHeadline.url } target='_blank'>{ realHeadline.url }</a></div>
             </div>
           }
         </div>
