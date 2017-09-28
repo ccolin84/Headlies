f8f9c26a4b843a68d084bd6fbd7edd9e3336b552
ccolin84 <ccolin84@gmail.com>
2017-09-28 13:58:22 -0400

    update server logging

--- Headlies/server/util/headlineHelpers.js
+++ Headlies/server/util/headlineHelpers.js
@@ -7,6 +7,7 @@ const wordpos = new WordPOS();
 const generateHeadlines = (source = 'bloomberg') => {
   return getHeadLines(source)
     .then((headlinesData) => {
+      console.log('got headlinesData: ', JSON.stringify(headlinesData));
       let titles = headlinesData.articles.map((article) => article.title);
       let titlesText = titles.reduce((acc, titleText) => acc + ' ' + titleText);
       return {pos: wordpos.getPOS(titlesText), titles, headlinesData};
